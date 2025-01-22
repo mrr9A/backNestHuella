3a38ad1b7169e5f565de4e48ca6073364d1aa5f9
mrr9A <romero123mar123@gmail.com>
2025-01-22 15:43:16 -0600

    Hola3

--- src/digital-personal/digital-personal.controller.ts
+++ src/digital-personal/digital-personal.controller.ts
@@ -1,41 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { DigitalPersonalService } from './digital-personal.service';
 import { CreateDigitalPersonalDto } from './dto/create-digital-personal.dto';
 import { UpdateDigitalPersonalDto } from './dto/update-digital-personal.dto';
-import { AcquisitionStarted, AcquisitionStopped, DeviceConnected, DeviceDisconnected, FingerprintReader, SampleFormat, SamplesAcquired } from '@digitalpersona/devices';
-import { BioSample, BioFactor, BioSampleEncryption, BioSampleFormat, BioSampleFormatOwner, BioSampleHeader, Base64UrlString, BioSamplePurpose, BioSampleType, Credential, CredentialId, Answer, Answers, Base32, Base32String, Base64, Base64String, Base64Url, ClaimName, ClaimNames, ClaimSet, CredentialUsed, FaceImage, FaceImageType, HexString } from '@digitalpersona/core';
-import { FingerprintsAuth, ContactlessCardAuth, EmailOtpAuth, FaceAuth, Finger, FingerPosition, Fingers, PasswordAuth, PinAuth, ProximityCardAuth, PushOtpAuth, SecurityQuestionsAuth, SmartCardAuth, SmsOtpAuth, TimeOtpAuth, U2FAuth, WindowsAuth } from '@digitalpersona/authentication';
-import { ContactlessCardEnroll, EnrollmentContext, FaceEnroll, FingerprintsEnroll, PasswordEnroll, PinEnroll, ProximityCardEnroll, QuestionWithAnswer, SecurityQuestionsEnroll, SmartCardEnroll, SmartCardEnrollmentData, TimeOtpEnroll, U2FEnroll } from '@digitalpersona/enrollment'
-import { ADAttributeName, AdminService, Attribute, AttributeAction, AttributeName, AuthService, AuthenticationData, AuthenticationHandle, AuthenticationStatus, ClaimRequest, ClaimsService, ContextualInfo, DatabaseType, EnrollService, ExtendedAuthResult, IAdminService, IAuthService, IAuthenticationClient, IClaimsService, IEnrollService, IPolicyService, ISecretService, IService, LDSAttributeName, LicenseInfo, LicenseType, PSKCOutput, Policy, PolicyElement, PolicyInfo, PolicyService, PolicyTrigger, ResourceActions, SearchQuery, SearchScope, SecretService, ServerSettingType, ServerSettings, ServiceError, ServiceFault, TriggerName, TriggerNames, UACFlags, UserAccountType, UserInfo, VarBlob, VarBool, VarData, VarInt, VarString, VarType } from '@digitalpersona/services'
-
 
 @Controller('digital-personal')
 export class DigitalPersonalController {
-  constructor(private readonly digitalPersonalService: DigitalPersonalService,
-    private readonly authService: AuthService
-  ) { }
-
-  /*   @Post()
-    create(@Body() createDigitalPersonalDto: CreateDigitalPersonalDto) {
-      return this.digitalPersonalService.create(createDigitalPersonalDto);
-    } */
+  constructor(private readonly digitalPersonalService: DigitalPersonalService) {}
 
   @Post()
-  async login(@Body() loginDto: { fingerprint: string }) {
-    try {
-      const fingerprintTemplate:any = Buffer.from(loginDto.fingerprint, 'base64');
-
-      // Llamar al servicio de autenticación
-      const result = await this.authService.ContinueAuthentication(1,fingerprintTemplate);
-
-      if (result) {
-        return { message: 'Autenticación exitosa' };
-      } else {
-        throw new HttpException('Autenticación fallida', HttpStatus.UNAUTHORIZED);
-      }
-    } catch (error) {
-      throw new HttpException('Error en autenticación', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  create(@Body() createDigitalPersonalDto: CreateDigitalPersonalDto) {
+    return this.digitalPersonalService.create(createDigitalPersonalDto);
   }
 
   @Get()
